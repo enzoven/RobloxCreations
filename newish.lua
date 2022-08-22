2b76e5dcd460bfa500e89ce757fcfe77291a1d86
Enzo Vendramini <69326745+enzoven@users.noreply.github.com>
2022-08-22 11:40:09 -0300

    Update and rename novo.lua to newish.lua

---
+++ newish.lua
@@ -0,0 +1,5 @@
+char = script.parent -- Dummy
+
+wait(math.Random(5,10) -- Wait time (Not important)
+
+-- Effect script goes here --
--- novo.lua
+++
@@ -1,5 +0,0 @@
-char = script.parent -- Dummy
-
-wait(math.Random(5,10) -- Tempo de espera aleatório entre 5 e 10 segundos para o efeito se aplicar (Opcional)
-
--- Script do efeito --
