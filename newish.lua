eaf82628c7f63860123ea1c9d09fa9889d6e78a4
Enzo Vendramini <69326745+enzoven@users.noreply.github.com>
2022-08-22 11:14:46 -0300

    Update newish.lua

--- newish.lua
+++ newish.lua
@@ -1,1 +1,5 @@
-char = script.parent
+char = script.parent -- Dummy
+
+wait(math.Random(5,10) -- Tempo de espera aleatório entre 5 e 10 segundos para o efeito se aplicar (Opcional)
+
+-- Script do efeito --
