d9ee83682ecd7506f350b3bf18c41f3d4bd7461a
Enzo Vendramini Paggiaro <e2enzo2@gmail.com>
2022-09-28 09:15:06 -0300

    Create bruhmella.lua

---
+++ bruhmella.lua
@@ -0,0 +1,6 @@
+char = script.Parent.Parent -- Dummy
+local ChatService = game:GetService("Chat") -- Chat
+
+wait(math.Random(3,5) -- Wait time (Optional)
+
+ChatService:Chat(char.Head,"bruh","White") -- bruh
