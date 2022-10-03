dfded84df3f1be8fddbbf82c1d88321244144f94
Enzo Vendramini Paggiaro <e2enzo2@gmail.com>
2022-10-03 12:22:39 -0300

    Delete bruhmella.lua

--- bruhmella.lua
+++
@@ -1,6 +0,0 @@
-char = script.Parent.Parent -- Dummy
-local ChatService = game:GetService("Chat") -- Chat
-
-wait(math.Random(3,5) -- Wait time (Optional)
-
-ChatService:Chat(char.Head,"bruh","White") -- bruh
