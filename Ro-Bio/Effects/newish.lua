cbce6835433ae9abadf7a4f5cf5c3b7cef03b6a4
Enzo Vendramini Paggiaro <e2enzo2@gmail.com>
2022-10-03 12:21:39 -0300

    Add files via upload

---
+++ Ro-Bio/Effects/newish.lua
@@ -0,0 +1,5 @@
+char = script.Parent.Parent -- Dummy
+
+wait(math.Random(5,10) -- Wait time (Optional)
+
+-- Effect script goes here --
---
+++ Ro-Bio/Useful/gamepassvirus.lua
@@ -0,0 +1,16 @@
+-- Put inside virus model
+
+local GPassId = 0 -- Replace 0 with your gamepass ID
+
+script.Parent.ClickDetector.MouseClick:connect(function(plr)
+    if game.MarketplaceService:UserOwnsGamePassAsync(plr.UserId, GPassId) then -- If player owns the gamepass
+      
+      script.Parent.Sound:Play()
+      plr.Backpack.Needlegun.Needle.bio.Virus.Value = game.Lighting.Virus -- Collect the effect
+      
+    else -- If player don't own the gamepass
+      
+      game.MarketplaceService:PromptGamePassPurchase(plr, GPassId) -- Buy button appears
+    
+    end
+  end)
